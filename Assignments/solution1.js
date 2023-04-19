036e97ac935ed2c655bc244db3e09084c29555a4
OkelloSam21 <osamuel21@student.kibu.ac.ke>
2023-04-19 20:23:07 +0300

    Added solution1 file

--- Assignments/solution1.js
+++ Assignments/solution1.js
@@ -1,8 +1,11 @@
 "Use strict";
 // == is used when comparing  values with same data types
 let number1 = 43
-let number2 = '43'
+let number2 = 43
 
-console.log('number1 == number2' + number1 === number2)
+console.log('number1 == number2' + number1 == number2)
 
 //'===' when comparing variables of same data type
+var number = `54`
+var num = `54`
+console.log('number === num' + number === num)

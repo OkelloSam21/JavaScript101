53885dade7d067f8fdbbff4ceb6f8504c0f3a1bb
SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
2023-04-21 21:17:40 +0000

    update

--- Assignments/arraysAndArraysMethods.js
+++ Assignments/arraysAndArraysMethods.js
@@ -5,7 +5,7 @@
 
 const array  = ["Javascript", "Html", "Css", "Java"]
 
-// Splice 
+// Slice 
 
 let slice = array.slice(1)
 console.log(slice)  // [ 'Html', 'Css', 'Java' ]
@@ -54,9 +54,17 @@ array.forEach(printArrayElements)
     console.log(POP)
 
 // push
+let city = ["Nairob", "Kisumu", "Nakuru"];
 
+// add "Mombasa" to the array
+city.push("Mombasa");
+
+console.log(city);
 
 // splice
+let prime_numbers = [2, 3, 5, 7, 9, 11];
 
-
-
+// replace 1 element from index 4 by 13
+let removedElement = prime_numbers.splice(4, 1, 13);
+console.log(removedElement);
+console.log(prime_numbers);
